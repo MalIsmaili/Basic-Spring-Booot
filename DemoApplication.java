eeceedb98c3af1c47f053ddb954557665a03f100
MalIsmaili <malismaili99@gmail.com>
2024-08-13 11:44:49 +0200

    Add files via upload
    First version of this code

---
+++ DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    // New endpoint to sum two numbers
+    @GetMapping("/sum/{a}/{b}")
+    public String sum(@PathVariable("a") int a, @PathVariable("b") int b) {
+        int result = a + b;
+        return String.format("%d + %d = %d", a, b, result);
+    }
+}
+
